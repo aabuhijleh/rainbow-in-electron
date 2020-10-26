f3c160e7fe4baa22b592acb6f752ee019a7a6e62
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-10-26 16:06:12 +0200

    feat: add rainbow-web-sdk to preload script

--- src/preload.ts
+++ src/preload.ts
@@ -1,12 +1,11 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+// to make node work with ES6 modules
+import esm from "esm";
+require = esm(module);
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
+// because we are in the preload script
+// wait until their deps have been loaded
+window.addEventListener("DOMContentLoaded", () => {
+  const rainbow = require("rainbow-web-sdk").default;
+  // console.log("rainbowSdk", rainbow);
+  // you can now use the sdk
 });
